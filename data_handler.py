4cb616658ba01b92e05fb0c63bfdfd27a818769c
mack-wilson <93164203+mack-wilson@users.noreply.github.com>
2023-04-24 19:09:47 -0700

    fill out data_handler

--- data_handler.py
+++ data_handler.py
@@ -2,17 +2,37 @@
 import csv
 
 def load_job_applications(file_name):
-    # Load job applications from the CSV
-    pass
+    job_applications = []
+    with open(file_name, mode='r', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            job_applications.append(row)
+    return job_applications
 
 def add_job_application(file_name, application_data):
-    # Add a new application to the CSV
-    pass
+    fieldnames = ['job_title', 'company', 'source', 'job_link', 'salary_estimate']
+    with open(file_name, mode='a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(application_data)
 
 def update_job_application(file_name, application_data, index):
-    # Update an existing application in the CSV
-    pass
+    job_applications = load_job_applications(file_name)
+    job_applications[index] = application_data
+
+    fieldnames = ['job_title', 'company', 'source', 'job_link', 'salary_estimate']
+    with open(file_name, mode='w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for app_data in job_applications:
+            writer.writerow(app_data)
 
 def delete_job_application(file_name, index):
-    # Delete an application from the CSV
-    pass
+    job_applications = load_job_applications(file_name)
+    del job_applications[index]
+
+    fieldnames = ['job_title', 'company', 'source', 'job_link', 'salary_estimate']
+    with open(file_name, mode='w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for app_data in job_applications:
+            writer.writerow(app_data)
