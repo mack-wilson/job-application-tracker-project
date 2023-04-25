0d45716fa1833f5ecf92f62724bff752a5a47d79
mack-wilson <93164203+mack-wilson@users.noreply.github.com>
2023-04-24 18:07:58 -0700

    init data_handler

---
+++ data_handler.py
@@ -0,0 +1,18 @@
+# data_handler.py
+import csv
+
+def load_job_applications(file_name):
+    # Load job applications from the CSV
+    pass
+
+def add_job_application(file_name, application_data):
+    # Add a new application to the CSV
+    pass
+
+def update_job_application(file_name, application_data, index):
+    # Update an existing application in the CSV
+    pass
+
+def delete_job_application(file_name, index):
+    # Delete an application from the CSV
+    pass
